a910eb94e662a44368879d9a9a671cf24136a5ad
Rick Strahl <rstrahl@west-wind.com>
2016-05-28 01:06:44 -0700

    Add global Exception handler

--- src/AlbumViewerNetCore/Startup.cs
+++ src/AlbumViewerNetCore/Startup.cs
@@ -13,6 +13,9 @@ using AlbumViewerBusiness;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting.Internal;
 using System.IO;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
 
 namespace AlbumViewerNetCore
 {
@@ -94,8 +97,34 @@ namespace AlbumViewerNetCore
                 //app.UseBrowserLink();
             }
             else
-            {
-                app.UseExceptionHandler("/");
+            {                
+                app.UseExceptionHandler(errorApp =>
+
+                    errorApp.Run(async (context) =>
+                    {
+                        context.Response.StatusCode = 500;
+                        context.Response.ContentType = "text/html";
+                        await context.Response.WriteAsync("<html><body>\r\n");
+                        await
+                            context.Response.WriteAsync(
+                                "We're sorry, we encountered an un-expected issue with your application.<br>\r\n");
+
+                        // Capture the exception
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if (error != null)
+                        {
+                            // This error would not normally be exposed to the client
+                            await
+                                context.Response.WriteAsync("<br>Error: " +
+                                                            HtmlEncoder.Default.Encode(error.Error.Message) + "<br>\r\n");
+                        }
+                        await context.Response.WriteAsync("<br><a href=\"/\">Home</a><br>\r\n");
+                        await context.Response.WriteAsync("</body></html>\r\n");
+                        await context.Response.WriteAsync(new string(' ', 512)); // Padding for IE
+                    }));
+
+
+                //app.UseExceptionHandler("/");
                 //app.UseExceptionHandler("/Home/Error");
             }
             
