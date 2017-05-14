1ed52163defd3d2a0e88be14148553ec021d021e
Rick Strahl <rstrahl@west-wind.com>
2017-05-14 00:53:41 -0700

    *** BROKEN BUILD Build is broken for Authentication - don't know how to hook up Cookie Authentication in this new version.

--- src/AlbumViewerNetCore/Startup.cs
+++ src/AlbumViewerNetCore/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json.Serialization;
 using Serilog;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace AlbumViewerNetCore
 {
@@ -57,9 +58,19 @@ namespace AlbumViewerNetCore
                 }
             });
 
-            services.AddAuthentication();
-
-            services.AddCors(options =>
+			services.AddAuthentication(o =>
+		    {
+				//var opt = new CookieAuthenticationOptions()
+				//{
+				//	LoginPath = "/api/login",
+				//	LogoutPath = "/api/logout",
+				//};
+				
+			   o.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+			   o.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+		    });
+		
+			services.AddCors(options =>
             {
                 options.AddPolicy("CorsPolicy",
                     builder => builder
@@ -115,76 +126,70 @@ namespace AlbumViewerNetCore
                     .WriteTo.RollingFile(pathFormat: "logs\\log-{Date}.log")
                     .CreateLogger();
 
-            if (env.IsDevelopment())
-            {
-                loggerFactory.WithFilter(new FilterLoggerSettings
-                    {
-                        {"Trace", LogLevel.Trace},
-                        {"Default", LogLevel.Trace},
-                        {"Microsoft", LogLevel.Warning}, // very verbose
+			if (env.IsDevelopment())
+			{
+				loggerFactory.WithFilter(new FilterLoggerSettings
+					{
+						{"Trace", LogLevel.Trace},
+						{"Default", LogLevel.Trace},
+						{"Microsoft", LogLevel.Warning}, // very verbose
                         {"System", LogLevel.Warning}
-                    })
-                    .AddConsole()
-                    .AddSerilog();
+					})
+				.AddConsole()
+				.AddSerilog();
 
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                loggerFactory.WithFilter(new FilterLoggerSettings
-                    {
-                        {"Trace", LogLevel.Trace},
-                        {"Default", LogLevel.Trace},
-                        {"Microsoft", LogLevel.Warning}, // very verbose
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				loggerFactory.WithFilter(new FilterLoggerSettings
+					{
+						{"Trace", LogLevel.Trace},
+						{"Default", LogLevel.Trace},
+						{"Microsoft", LogLevel.Warning}, // very verbose
                         {"System", LogLevel.Warning}
-                    })
-                    .AddSerilog();
+					})
+					.AddSerilog();
 
-                app.UseExceptionHandler(errorApp =>
+				app.UseExceptionHandler(errorApp =>
 
-                    // Application level exception handler here - this is just a place holder
-                        errorApp.Run(async (context) =>
-                        {
-                            context.Response.StatusCode = 500;
-                            context.Response.ContentType = "text/html";
-                            await context.Response.WriteAsync("<html><body>\r\n");
-                            await context.Response.WriteAsync(
-                                    "We're sorry, we encountered an un-expected issue with your application.<br>\r\n");
+						// Application level exception handler here - this is just a place holder
+						errorApp.Run(async (context) =>
+						{
+							context.Response.StatusCode = 500;
+							context.Response.ContentType = "text/html";
+							await context.Response.WriteAsync("<html><body>\r\n");
+							await context.Response.WriteAsync(
+									"We're sorry, we encountered an un-expected issue with your application.<br>\r\n");
 
-                            // Capture the exception
-                            var error = context.Features.Get<IExceptionHandlerFeature>();
-                            if (error != null)
-                            {
-                                // This error would not normally be exposed to the client
-                                await
-                                    context.Response.WriteAsync("<br>Error: " +
-                                                                HtmlEncoder.Default.Encode(error.Error.Message) +
-                                                                "<br>\r\n");
-                            }
-                            await context.Response.WriteAsync("<br><a href=\"/\">Home</a><br>\r\n");
-                            await context.Response.WriteAsync("</body></html>\r\n");
-                            await context.Response.WriteAsync(new string(' ', 512)); // Padding for IE
-                        }));
+							// Capture the exception
+							var error = context.Features.Get<IExceptionHandlerFeature>();
+							if (error != null)
+							{
+								// This error would not normally be exposed to the client
+								await
+									context.Response.WriteAsync("<br>Error: " +
+																HtmlEncoder.Default.Encode(error.Error.Message) +
+																"<br>\r\n");
+							}
+							await context.Response.WriteAsync("<br><a href=\"/\">Home</a><br>\r\n");
+							await context.Response.WriteAsync("</body></html>\r\n");
+							await context.Response.WriteAsync(new string(' ', 512)); // Padding for IE
+						}));
 
-                //loggerFactory.AddConsole();
-                //app.UseExceptionHandler("/");
-                //app.UseExceptionHandler("/Home/Error");
-            }
+				//loggerFactory.AddConsole();
+				//app.UseExceptionHandler("/");
+				//app.UseExceptionHandler("/Home/Error");
+			}
 
             Console.WriteLine("\r\nPlatform: " + System.Runtime.InteropServices.RuntimeInformation.OSDescription);
 												string useSqLite = Configuration["Data:useSqLite"];
 												Console.WriteLine(useSqLite == "true" ? "SqLite" : "Sql Server");
-				
 
-            // Enable Cookie Auth with automatic user policy
-            app.UseCookieAuthentication(new CookieAuthenticationOptions()
-            {
-                AutomaticAuthenticate = true,
-                AutomaticChallenge = false,
-                LoginPath = "/api/login"
-            });
 
-            app.UseDatabaseErrorPage();
+			app.UseAuthentication();
+		
+		    app.UseDatabaseErrorPage();
             app.UseStatusCodePages();
 
             app.UseDefaultFiles(); // so index.html is not required
