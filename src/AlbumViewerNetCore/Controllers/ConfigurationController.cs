e11d5b82d0c2abe609030089e3af54e7d3f5df44
Rick Strahl <rstrahl@west-wind.com>
2017-10-17 16:30:59 -1000

    Add separate configuration controller that holds admin and information tasks.

---
+++ src/AlbumViewerNetCore/Controllers/ConfigurationController.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AlbumViewerAspNetCore;
+using AlbumViewerBusiness.Configuration;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AlbumViewerNetCore.Controllers
+{
+    [ServiceFilter(typeof(ApiExceptionFilter))]
+    [EnableCors("CorsPolicy")]
+    public class ConfigurationController : Controller
+    {
+        IOptions<ApplicationConfiguration> AppConfiguration;
+        IHostingEnvironment Host;
+
+        public ConfigurationController(IOptions<ApplicationConfiguration> configuration,
+            IHostingEnvironment host)
+        {
+            AppConfiguration = configuration;
+            Host = host;
+        }
+
+
+        [HttpGet]
+        [Route("api/configuration")]
+        public ApplicationConfiguration GetConfiguration()
+        {
+            return AppConfiguration.Value;
+        }
+
+
+	    [HttpGet("api/applicationstats")]
+	    public object GetApplicationStats()
+	    {
+		    var stats = new
+		    {
+			    OsPlatform = System.Runtime.InteropServices.RuntimeInformation.OSDescription,
+			    HostName = System.Environment.MachineName,
+			    Ip = HttpContext.Connection.LocalIpAddress.ToString()
+		    };
+
+		    return stats;
+	    }
+
+
+        /// <summary>
+        /// Allow Lets Encrypt Reneawal requests through the pipeline by 
+        /// default these are twarthed because they are treated as extensionless
+        /// action route urls (which have no match and fail).
+        /// 
+        /// Explicitly map a route to the URL and then serve the physical file
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route(".well-known/acme-challenge/{id}")]
+        public ActionResult LetsEncrypt(string id)
+        {
+            var file = Path.Combine(Host.WebRootPath, ".well-known", "acme-challenge", id);
+            return PhysicalFile(file, "text/plain");
+        }
+    }
+}
