598c0f6ac692301fbca0574e2b37679342024072
Rick Strahl <rstrahl@west-wind.com>
2017-10-22 00:59:27 -1000

    Run docker file with fixed console not in background.

--- src/AlbumViewerNetCore/dockerbuild.ps1
+++ src/AlbumViewerNetCore/dockerbuild.ps1
@@ -12,7 +12,7 @@ docker build -t westwind/albumviewer:albumviewer .
 $localIpAddress=((ipconfig | findstr [0-9].\.)[0]).Split()[-1]
 
 # immediately start running the container in the background (-d) (no console)
-docker run  -d -it -p 5004:5000 --name albumviewer  westwind/albumviewer:albumviewer 
+docker run  -it -p 5004:5000 --name albumviewer  westwind/albumviewer:albumviewer 
 
 # Map host IP to a domain - so we can access local SQL server
 #--add-host dev.west-wind.com:$localIpAddress
